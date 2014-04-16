0f7132a3396587bbb811f6931ea1520aa4835277
Eric Galloway <ericgalloway@gmail.com>
2014-04-15 18:36:56 -0700

    Adding magic startup files

---
+++ start_newtabmagic.py
@@ -0,0 +1,15 @@
+""" 
+Command Line Usage: 
+    python -m IPython start_magic.py -i
+  
+Interactive Usage:
+    In [1]: from start_newtabmagic import *
+    Starting job # 0 in a separate thread.
+    Server running at http://127.0.0.1:8889/
+
+"""
+ip = get_ipython()
+ip.run_cell('%load_ext newtabmagic')
+ip.run_cell('%newtab --port 8889')
+ip.run_cell('%newtab --browser firefox')
+ip.run_cell('%newtab --server start')

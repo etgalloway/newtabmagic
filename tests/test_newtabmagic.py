949db02418c56a7b86e3c2e6759c441b79ad2eb2
Eric Galloway <ericgalloway@gmail.com>
2014-04-30 12:28:56 -0700

    Adding port argument tests

--- tests/test_newtabmagic.py
+++ tests/test_newtabmagic.py
@@ -6,10 +6,47 @@ To run tests:
     nosetests test_newtabmagic.py
 
 '''
+# pylint: disable=C0111, C0321, R0903
+import contextlib
 import nose
+import sys
 
 import IPython
 import newtabmagic
+
+
+if sys.version_info.major == 2:
+    from StringIO import StringIO
+else:
+    from io import StringIO
+
+
+if not IPython.get_ipython():
+    from IPython.testing import globalipapp
+    globalipapp.start_ipython()
+
+
+@contextlib.contextmanager
+def stdout_redirected(new_target=None):
+    old_target = sys.stdout
+    if new_target is None:
+        sys.stdout = StringIO()
+    else:
+        sys.stdout = new_target
+    try:
+        yield sys.stdout
+    finally:
+        sys.stdout = old_target
+
+
+@contextlib.contextmanager
+def server_running(newtab):
+    newtab.newtab('--server start')
+    try:
+        yield
+    finally:
+        newtab.newtab('--server stop')
+
 
 def get_newtabmagic(new_tabs_enabled=False, browser=None, port=None):
     ip = IPython.get_ipython()
@@ -23,7 +60,6 @@ def get_newtabmagic(new_tabs_enabled=False, browser=None, port=None):
 
 
 def test_set_browser():
-
     paths = ['chrome',
         'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
         'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe']
@@ -36,3 +72,31 @@ def test_set_browser():
             result = newtab.browser
             expected = path
             nose.tools.assert_equals(result, expected)
+
+
+def test_set_port():
+    newtab = get_newtabmagic(browser='firefox')
+
+    root_original = newtab.base_url()
+    newtab.newtab('--port 9999')
+    result = newtab.base_url()
+    expected = 'http://127.0.0.1:9999/'
+    assert result != root_original
+    nose.tools.assert_equals(result, expected)
+
+
+def test_set_port_server_running():
+    # Setting the port number should fail if the server is running
+
+    newtab = get_newtabmagic(browser='firefox', port=9999)
+    root_original = newtab.base_url()
+    with server_running(newtab):
+        with stdout_redirected() as out:
+            newtab.newtab('--port 8888')
+
+    result = out.getvalue()
+    expected = 'Server already running. Port number not changed\n'
+    nose.tools.assert_equals(result, expected)
+    result = newtab.base_url()
+    expected = root_original
+    nose.tools.assert_equals(result, expected)

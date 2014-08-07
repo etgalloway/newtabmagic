239ea4965444bbaf7f73a11fd5e6141581677493
Eric Galloway <ericgalloway@gmail.com>
2014-08-06 20:17:01 -0700

    Modify startup files
    Remove server start commands. Change port number.  Change browser to
    Chrome on Windows.

--- startup/newtabmagic_startup.py
+++ startup/newtabmagic_startup.py
@@ -1,5 +1,4 @@
 ip = get_ipython()
 ip.run_cell('%load_ext newtabmagic')
-ip.run_cell('%newtab --port 8889')
-ip.run_cell('%newtab --browser firefox')
-ip.run_cell('%newtab --server start')
+ip.run_cell('%newtab --port 8880')
+ip.run_cell('%newtab --browser C:\Program Files (x86)\Google\Chrome\Application\chrome.exe')
